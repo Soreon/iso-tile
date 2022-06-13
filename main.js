f9f71be9220ea9bec0135b8a55d367ce431be21e
Jean Bachelet <j.bachelet@waigeo.fr>
2022-06-13 15:24:44 +0200

    Ajout de controles

--- main.js
+++ main.js
@@ -2,22 +2,23 @@ const canvas = document.querySelector('#canvas');
 const context = canvas.getContext('2d');
 context.imageSmoothingEnabled = false;
 
-const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 600;
-const BASE_TILE_SIZE = 32;
-const X_ORIGIN = (CANVAS_WIDTH - BASE_TILE_SIZE) / 2;
-const Y_ORIGIN = CANVAS_HEIGHT / 4;
-const TILE_ZOOM = 1;
-const EFFECTIVE_TILE_SIZE = BASE_TILE_SIZE * TILE_ZOOM;
-const VECT_I = [0.5, 0.25];
-const VECT_J = [-0.5, 0.25];
-const SQRT_NUMBER_OF_CUBES = 24;
-const WAVE_AMPLITUDE = 20;
-const WAVE_X_COMPONENT = 100; // Négatif pour inverser les sens de la vague
-const WAVE_Y_COMPONENT = 50;
-const WAVE_FREQUENCY = 300;
+const _ = window;
+_.CANVAS_WIDTH = 800;
+_.CANVAS_HEIGHT = 600;
+_.BASE_TILE_SIZE = 32;
+_.X_ORIGIN = (_.CANVAS_WIDTH - _.BASE_TILE_SIZE) / 2;
+_.Y_ORIGIN = _.CANVAS_HEIGHT / 4;
+_.TILE_ZOOM = 1;
+_.EFFECTIVE_TILE_SIZE = _.BASE_TILE_SIZE * _.TILE_ZOOM;
+_.VECT_I = [0.5, 0.25];
+_.VECT_J = [-0.5, 0.25];
+_.SQRT_NUMBER_OF_CUBES = 24;
+_.WAVE_AMPLITUDE = 20;
+_.WAVE_X_COMPONENT = 100; // Négatif pour inverser les sens de la vague
+_.WAVE_Y_COMPONENT = 50;
+_.WAVE_FREQUENCY = 300;
 
-const cubeImage = new Image(BASE_TILE_SIZE, BASE_TILE_SIZE);
+const cubeImage = new Image(_.BASE_TILE_SIZE, _.BASE_TILE_SIZE);
 cubeImage.src = 'cube.png';
 const cubeImageLoaded = new Promise((resolve) => { cubeImage.onload = () => { resolve(); }; });
 
@@ -29,31 +30,31 @@ let selectionI = 5;
 let selectionJ = 14;
 
 function IJToIsoXY(i, j) {
-  let vi = math.multiply(VECT_I, i);
-  let vj = math.multiply(VECT_J, j);
+  let vi = math.multiply(_.VECT_I, i);
+  let vj = math.multiply(_.VECT_J, j);
 
   [vi, vj] = math.add(vi, vj);
 
-  vi = X_ORIGIN + vi * (EFFECTIVE_TILE_SIZE);
-  vj = Y_ORIGIN + vj * (EFFECTIVE_TILE_SIZE);
+  vi = _.X_ORIGIN + vi * (_.EFFECTIVE_TILE_SIZE);
+  vj = _.Y_ORIGIN + vj * (_.EFFECTIVE_TILE_SIZE);
 
   return { vi, vj };
 }
 
 function draw() {
-  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  for (let i = 0; i < SQRT_NUMBER_OF_CUBES; i += 1) {
-    for (let j = 0; j < SQRT_NUMBER_OF_CUBES; j += 1) {
+  context.clearRect(0, 0, _.CANVAS_WIDTH, _.CANVAS_HEIGHT);
+  for (let i = 0; i < _.SQRT_NUMBER_OF_CUBES; i += 1) {
+    for (let j = 0; j < _.SQRT_NUMBER_OF_CUBES; j += 1) {
       const conv = IJToIsoXY(i, j);
       const { vi } = conv;
       let { vj } = conv;
 
-      const v = Math.sin((+new Date() + ((i * WAVE_X_COMPONENT) + (j * WAVE_Y_COMPONENT))) / WAVE_FREQUENCY) * WAVE_AMPLITUDE;
+      const v = Math.sin((+new Date() + ((i * _.WAVE_X_COMPONENT) + (j * _.WAVE_Y_COMPONENT))) / _.WAVE_FREQUENCY) * _.WAVE_AMPLITUDE;
       vj += v;
 
-      context.drawImage(cubeImage, 0, 0, BASE_TILE_SIZE, BASE_TILE_SIZE, vi, vj, EFFECTIVE_TILE_SIZE, EFFECTIVE_TILE_SIZE);
+      context.drawImage(cubeImage, 0, 0, _.BASE_TILE_SIZE, _.BASE_TILE_SIZE, vi, vj, _.EFFECTIVE_TILE_SIZE, _.EFFECTIVE_TILE_SIZE);
 
-      if (i === selectionI && j === selectionJ) context.drawImage(highlightImage, 0, 0, BASE_TILE_SIZE, highlightImage.height, vi, vj - ((highlightImage.height * TILE_ZOOM) - EFFECTIVE_TILE_SIZE), EFFECTIVE_TILE_SIZE, highlightImage.height * TILE_ZOOM);
+      if (i === selectionI && j === selectionJ) context.drawImage(highlightImage, 0, 0, _.BASE_TILE_SIZE, highlightImage.height, vi, vj - ((highlightImage.height * _.TILE_ZOOM) - _.EFFECTIVE_TILE_SIZE), _.EFFECTIVE_TILE_SIZE, highlightImage.height * _.TILE_ZOOM);
     }
   }
 }
@@ -83,10 +84,10 @@ window.addEventListener('keydown', (e) => {
       break;
     default: break;
   }
-  selectionJ += SQRT_NUMBER_OF_CUBES;
-  selectionI += SQRT_NUMBER_OF_CUBES;
-  selectionJ %= SQRT_NUMBER_OF_CUBES;
-  selectionI %= SQRT_NUMBER_OF_CUBES;
+  selectionJ += _.SQRT_NUMBER_OF_CUBES;
+  selectionI += _.SQRT_NUMBER_OF_CUBES;
+  selectionJ %= _.SQRT_NUMBER_OF_CUBES;
+  selectionI %= _.SQRT_NUMBER_OF_CUBES;
 });
 
 // canvas.addEventListener('mousemove', (e) => {
@@ -97,3 +98,14 @@ window.addEventListener('keydown', (e) => {
 //   selectionI = conv.vi;
 //   selectionJ = conv.vj;
 // });
+
+document.querySelectorAll('#controls .slider').forEach((element) => {
+  const valueDisplay = element.parentElement.querySelector('.value');
+  const { variable } = element.dataset;
+  element.value = _[variable];
+  valueDisplay.textContent = element.value;
+  element.addEventListener('input', () => {
+    _[variable] = +element.value;
+    valueDisplay.textContent = element.value;
+  });
+});
